ea6473486162fa73c735a73f2bd5c8bae8071bab
Daniel Slatkin <danielslatkin@gmail.com>
2023-04-07 06:55:46 +0000

    fix ssr random weapon image flash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,39 @@
-<script>
-    const weapons = [
-        { filename: 'berdiche.jpg', alt: 'berdiche' },
-        { filename: 'claymore.jpg', alt: 'claymore' },
-        { filename: 'falchion.gif', alt: 'falchion' },
-        { filename: 'flamberge.jpg', alt: 'flamberge' },
-        { filename: 'gladius.jpg', alt: 'gladius' },
-        { filename: 'khyber.jpg', alt: 'khyber' },
-        { filename: 'rapier.jpg', alt: 'rapier' },
-        { filename: 'rapierparts.jpg', alt: 'rapierparts' },
-        { filename: 'stiletto.jpg', alt: 'stiletto' },
-        { filename: 'tegha.jpg', alt: 'tegha' },
-    ];
+<script lang="ts">
+    import { browser } from '$app/environment';
 
-    $: weaponIdx = Math.floor(Math.random() * weapons.length);
-    $: weapon = weapons[weaponIdx];
+    type Weapon = {
+        filename: string;
+        alt: string;
+    };
+
+    let weapons: Weapon[] | undefined = undefined;
+    if (browser) {
+        weapons = [
+            { filename: 'berdiche.jpg', alt: 'berdiche' },
+            { filename: 'claymore.jpg', alt: 'claymore' },
+            { filename: 'falchion.gif', alt: 'falchion' },
+            { filename: 'flamberge.jpg', alt: 'flamberge' },
+            { filename: 'gladius.jpg', alt: 'gladius' },
+            { filename: 'khyber.jpg', alt: 'khyber' },
+            { filename: 'rapier.jpg', alt: 'rapier' },
+            { filename: 'rapierparts.jpg', alt: 'rapierparts' },
+            { filename: 'stiletto.jpg', alt: 'stiletto' },
+            { filename: 'tegha.jpg', alt: 'tegha' },
+        ];
+    }
+
+    let weapon: Weapon | undefined = undefined;
+    if (browser && weapons) {
+        const idx = Math.floor(Math.random() * weapons.length);
+        weapon = weapons[idx];
+    }
 </script>
 
-<a href="images/pics/large/{weapon.filename}" target="_blank">
-    <img src="images/pics/thumbnail/{weapon.filename}" alt="A {weapon.alt}" />
-</a>
+{#if weapon}
+    <a href="images/pics/large/{weapon.filename}" target="_blank">
+        <img
+            src="images/pics/thumbnail/{weapon.filename}"
+            alt="A {weapon.alt}"
+        />
+    </a>
+{/if}
