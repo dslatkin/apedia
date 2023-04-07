40e62074f80f6abbb1494c27acdf6ddae5e5cd8d
Daniel Slatkin <danielslatkin@gmail.com>
2023-04-07 06:44:57 +0000

    get static apedia site working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+    const weapons = [
+        { filename: 'berdiche.jpg', alt: 'berdiche' },
+        { filename: 'claymore.jpg', alt: 'claymore' },
+        { filename: 'falchion.gif', alt: 'falchion' },
+        { filename: 'flamberge.jpg', alt: 'flamberge' },
+        { filename: 'gladius.jpg', alt: 'gladius' },
+        { filename: 'khyber.jpg', alt: 'khyber' },
+        { filename: 'rapier.jpg', alt: 'rapier' },
+        { filename: 'rapierparts.jpg', alt: 'rapierparts' },
+        { filename: 'stiletto.jpg', alt: 'stiletto' },
+        { filename: 'tegha.jpg', alt: 'tegha' },
+    ];
+
+    $: weaponIdx = Math.floor(Math.random() * weapons.length);
+    $: weapon = weapons[weaponIdx];
+</script>
+
+<a href="images/pics/large/{weapon.filename}" target="_blank">
+    <img src="images/pics/thumbnail/{weapon.filename}" alt="A {weapon.alt}" />
+</a>
